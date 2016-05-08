e812bf30eadfeaddbd864ec3a20f0d6bada9b9cf
BQC <bqcuong2212@gmail.com>
2016-05-08 14:36:56 +0700

    test upload

--- src/main/java/uet/jcia/shop/admin/controller/UploadService.java
+++ src/main/java/uet/jcia/shop/admin/controller/UploadService.java
@@ -1,38 +1,105 @@
 package uet.jcia.shop.admin.controller;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+import uet.jcia.shop.controller.MessageType;
 
 /**
  * Servlet implementation class UploadService
  */
 @WebServlet("/admin/UploadService")
+@MultipartConfig(
+		fileSizeThreshold = 2 * 1024 * 1024,
+		maxFileSize = 10 * 1024 * 1024,
+		maxRequestSize = 50 * 1024 * 1024)
 public class UploadService extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
+	private static String productDataPath;
+	
     public UploadService() {
         super();
-        // TODO Auto-generated constructor stub
+    }
+    
+    @Override
+    public void init() throws ServletException {
+    	try {
+			ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+			InputStream is = classLoader.getResourceAsStream("path.properties");
+			Properties p = new Properties();
+			p.load(is);
+			productDataPath = p.getProperty("productData");
+			is.close();
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+			
+		}
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String fileName = null;
+		
+		try {
+			for (Part p : request.getParts()) {
+				String fn = getFileName(p);
+				if (!fn.isEmpty()) {
+					fileName = fn;
+				}
+				p.write(productDataPath + fn);
+				
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println(productDataPath);
+		request.setAttribute("MessageType", MessageType.SUCCESS);
+		request.setAttribute("message", "upload successfully");
+		request.setAttribute("fileName", fileName);
+		
+		String requestFrom = request.getParameter("reqsrc");
+		if (requestFrom != null) {
+			forwardStream(request, response, requestFrom);
+			
+		} else {
+			forwardStream(request, response, "/admin/upload-result.jsp");
+		}
+	}
+	
+	private String getFileName(Part p) {
+		String contentDisp = p.getHeader("content-disposition");
+		String[] items = contentDisp.split(";");
+		
+		for (String s : items) {
+			if (s.trim().startsWith("filename")) {
+				return s.substring(s.indexOf("=") + 2, s.length() - 1);
+			}
+		}
+		
+		return "";
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	private void forwardStream(HttpServletRequest req, HttpServletResponse rsp, String destination)
+			throws ServletException, IOException {
+		
+		ServletContext context = getServletContext();
+		RequestDispatcher dispatcher = context.getRequestDispatcher(destination);
+		dispatcher.forward(req, rsp);
 	}
-
 }
---
+++ src/main/webapp/admin/test-upload.jsp
@@ -0,0 +1,47 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions"%>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Update Product</title>
+
+    <link href="../assests/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../assests/css/metisMenu.min.css" rel="stylesheet">
+    <link href="../assests/css/sb-admin-2.css" rel="stylesheet">
+    <link href="../assests/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+	<!-- header -->
+<jsp:include page="header.jsp"></jsp:include>
+        
+	<!-- content -->
+		<img alt="" src="../img/product/${fileName}">
+		<form action="UploadService" method="post"
+                        enctype="multipart/form-data">
+			<input type="file" name="file" size="2" />
+			<input type="hidden" name="reqsrc" value="/admin/test-upload.jsp"/>
+			<br />
+			<input type="submit" value="Upload File" />
+		</form>    
+	<!-- footer -->
+<jsp:include page="footer.jsp"></jsp:include>
+
+</body>
+</html>
